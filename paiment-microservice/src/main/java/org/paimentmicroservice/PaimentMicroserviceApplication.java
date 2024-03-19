25b51e3aea9d675cd7124eb34d63e6fcb3dac38c
AchrafLamsahel <Achraflamsahel1@gmail.com>
2024-03-19 20:12:22 +0000

    #1 :First-Commit

---
+++ paiment-microservice/src/main/java/org/paimentmicroservice/PaimentMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.paimentmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaimentMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaimentMicroserviceApplication.class, args);
+    }
+
+}
